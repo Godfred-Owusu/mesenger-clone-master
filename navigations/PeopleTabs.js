c29a8d2d8907af89d5e567530c9ce873d9d86687
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2023-09-11 20:18:15 +0100

    Removed the console logs

--- navigations/PeopleTabs.js
+++ navigations/PeopleTabs.js
@@ -1,98 +1,102 @@
 import React from "react";
 import { TouchableOpacity, View, StyleSheet, Animated } from "react-native";
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Stories from "../Screens/Stories";
 import Users from "../Screens/Users";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 
 const PeopleTabs = () => {
+  const Tab = createMaterialTopTabNavigator();
 
-    const Tab = createMaterialTopTabNavigator()
+  return (
+    <Tab.Navigator
+      tabBar={({ state, descriptors, navigation, position }) => {
+        return (
+          <View style={styles.topTabContainer}>
+            {state.routes.map((route, index) => {
+              const { options } = descriptors[route.key];
+              const label =
+                options.tabBarLabel !== undefined
+                  ? options.tabBarLabel
+                  : options.title !== undefined
+                  ? options.title
+                  : route.name;
 
-    return (
-        <Tab.Navigator
-            tabBar={({ state, descriptors, navigation, position }) => {
-                return (
-                    <View style={styles.topTabContainer}>
-                        {state.routes.map((route, index) => {
-                            const { options } = descriptors[route.key];
-                            const label =
-                                options.tabBarLabel !== undefined
-                                    ? options.tabBarLabel
-                                    : options.title !== undefined
-                                        ? options.title
-                                        : route.name;
+              const isFocused = state.index === index;
 
-                            const isFocused = state.index === index;
+              const onPress = () => {
+                const event = navigation.emit({
+                  type: "tabPress",
+                  target: route.key,
+                });
 
-                            const onPress = () => {
-                                console.log(route.name)
-                                const event = navigation.emit({
-                                    type: 'tabPress',
-                                    target: route.key,
-                                });
+                if (!isFocused && !event.defaultPrevented) {
+                  navigation.navigate(route.name);
+                }
+              };
 
-                                if (!isFocused && !event.defaultPrevented) {
-                                    navigation.navigate(route.name);
-                                }
-                            };
+              const onLongPress = () => {
+                navigation.emit({
+                  type: "tabLongPress",
+                  target: route.key,
+                });
+              };
 
-                            const onLongPress = () => {
-                                navigation.emit({
-                                    type: 'tabLongPress',
-                                    target: route.key,
-                                });
-                            };
+              return (
+                <TouchableOpacity
+                  key={index}
+                  accessibilityRole="button"
+                  accessibilityState={isFocused ? { selected: true } : {}}
+                  accessibilityLabel={options.tabBarAccessibilityLabel}
+                  testID={options.tabBarTestID}
+                  onPress={onPress}
+                  onLongPress={onLongPress}
+                  style={{
+                    ...styles.topTab,
+                    backgroundColor: isFocused
+                      ? "rgba(211,211,211,0.3)"
+                      : "white",
+                  }}
+                >
+                  <Animated.Text
+                    style={{
+                      ...styles.topTabTitle,
+                      color: isFocused ? "black" : "gray",
+                    }}
+                  >
+                    {label}
+                  </Animated.Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        );
+      }}
+    >
+      <Tab.Screen name="ActiveUsers" component={Users} />
+      <Tab.Screen name="Stories" component={Stories} />
+    </Tab.Navigator>
+  );
+};
 
-
-                            return (
-                                <TouchableOpacity
-                                    key={index}
-                                    accessibilityRole="button"
-                                    accessibilityState={isFocused ? { selected: true } : {}}
-                                    accessibilityLabel={options.tabBarAccessibilityLabel}
-                                    testID={options.tabBarTestID}
-                                    onPress={onPress}
-                                    onLongPress={onLongPress}
-                                    style={{ ...styles.topTab, backgroundColor: isFocused ? 'rgba(211,211,211,0.3)' : 'white' }}
-                                >
-                                    <Animated.Text style={{ ...styles.topTabTitle, color: isFocused ? 'black' : 'gray' }}>{label}</Animated.Text>
-                                </TouchableOpacity>
-                            );
-                        })}
-                    </View>)
-            }}
-
-        >
-            <Tab.Screen
-                name="ActiveUsers"
-                component={Users}
-            />
-            <Tab.Screen
-                name="Stories"
-                component={Stories}
-            />
-        </Tab.Navigator>
-    )
-}
-
-export default PeopleTabs
+export default PeopleTabs;
 
 const styles = StyleSheet.create({
-    topTabContainer: {
-        flexDirection: 'row',
-        paddingVertical: 20,
-        backgroundColor: 'white'
-    },
-    topTab: {
-        flex: 1, backgroundColor: 'red',
-        marginHorizontal: 10,
-        backgroundColor: 'rgba(211,211,211,0.3)',
-        borderRadius: 20,
-        alignItems: 'center'
-    },
-    topTabTitle: {
-        padding : 5,
-        fontSize: responsiveFontSize(1.8)
-    }
-})
+  topTabContainer: {
+    flexDirection: "row",
+    paddingVertical: 20,
+    backgroundColor: "white",
+  },
+  topTab: {
+    flex: 1,
+    backgroundColor: "red",
+    marginHorizontal: 10,
+    backgroundColor: "rgba(211,211,211,0.3)",
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  topTabTitle: {
+    padding: 5,
+    fontSize: responsiveFontSize(1.8),
+  },
+});
